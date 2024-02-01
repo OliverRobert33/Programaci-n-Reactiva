31e6221f7ec54ef51f7bd741a42909f67fb38c8a
Oliver Saraguro <rosaraguro@utpl.edu.ec>
2024-02-01 11:44:05 -0500

    Avanze

--- EjemploReactiva/ejemplo.js
+++ EjemploReactiva/ejemplo.js
@@ -1,18 +1,30 @@
 
-const {from, of} = require("rxjs");
-const {delay, concatMap, map} = require("rxjs/operators");
+const {from, of, zip} = require("rxjs");
+const {delay, concatMap} = require("rxjs/operators");
+
 
 const frutas = ["Pera", "Manzana", "Sandia", "Naranja"];
+const verduras = ["Lechuga", "Brocoli", "Pepino", "Coliflor", "Zanahoria"];
 
-//Nuestro observable => Emitira una fruta cada segundo 
+
+console.log("Voy a comprar futas.");
+
+// Obs 1 => Emitira una fruta cada segundo 
 const frutasObs = from(frutas).pipe(
     concatMap(item => of(item).pipe(delay(1000)))
 );
 
-console.log("Voy a comprar futas.");
+// Obs 2
+const verdurasObs = from(verduras);
 
-frutasObs.suscribe((fruta) => {
-    console.log(`Me llego una ${fruta}`)
-});
+// Obs 3 - Combinacion de los 3 
+const frutasYVerduras = zip(frutasObs, verdurasObs);
 
-console.log("Ya realize el pedido.");
+const disposable = frutasYVerduras.subscribe(frutaYVerdura => {
+    console.log(`Me llego una ${frutaYVerdura}`)
+})
+
+setTimeout(()=>{
+    console.log("Terminando Suscripcion");
+    disposable.unsubscribe();
+}, 5000)
